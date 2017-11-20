6509219db8a1a007d9487c82fac225cc612331fa
Xiaoxu Guo <ftiasch0@gmail.com>
2017-11-20 13:38:29 +0800

    updated to O(k log k) version

--- random_subset.cpp
+++ random_subset.cpp
@@ -1,42 +1,35 @@
-#include <cassert>
+#include "testlib.h"
 
-// binom{[n]}{k} : O(k \log^2 n)
+#include <cassert>
+#include <map>
+#include <vector>
+
+// binom{[n]}{k} : O(k \log k)
 template<typename Int>
 std::vector<Int> random_subset(Int n, int k)
 {
     assert(n >= k);
-    std::vector<Int> result;
-    std::map<Int, int> count;
-    auto at = [&count](Int index)
-    {
-        auto iterator = count.find(index);
-        return iterator == count.end() ? 0 : iterator->second;
-    };
-    for (auto i = 0; i < k; ++ i) {
-        auto rank = rnd.next(Int(1), n - i);
-        auto low = Int();
-        auto high = n - 1;
-        while (low < high) {
-            auto middle = low + high >> 1;
-            auto c = middle + 1;
-            for (auto p = middle; ~p; p -= ~p & p + 1) {
-                c -= at(p);
-            }
-            if (c >= rank) {
-                high = middle;
-            } else {
-                low = middle + 1;
-            }
+    std::set<Int> set;
+    for (Int i = n - k; i < n; ++ i) {
+        Int p = rnd.next(static_cast<Int>(0), i - 1);
+        if (set.count(p)) {
+            set.insert(i);
+        } else {
+            set.insert(p);
         }
-        for (auto p = high; p < n; p += ~p & p + 1) {
-            count[p] ++;
-        }
-        result.push_back(high);
     }
-    std::sort(result.begin(), result.end());
+    std::vector<Int> result;
+    for (auto elem : set) {
+        result.push_back(elem);
+    }
     return result;
 }
 
 int main()
 {
+    auto result = random_subset(1000000000LL, 10);
+    for (auto elem : result) {
+        printf("%lld,", elem);
+    }
+    puts("");
 }
